bf82a4b49a1a3dbf44ff4ddf55b433c35f16c500
sunnybysunny <1sunnymuniz@gmail.com>
2024-08-02 11:55:25 -0600

    added UUID generator

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './index.css'
 
+if (!crypto.randomUUID) {
+  (crypto as any).randomUUID = function (): string {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+          const r = Math.random() * 16 | 0;
+          const v = c === 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+      });
+  };
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
