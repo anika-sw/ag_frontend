202b6c55db98fb992d39c82528ca7741f687f92a
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2024-08-05 17:50:01 -0700

    reCAPTCHA running on localHost

--- vitetest.config.ts
+++ vitetest.config.ts
@@ -3,15 +3,27 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+    },
 });
 
-require('dotenv').config();
+// const webpack = require('webpack');
+// const dotenv = require('dotenv');
+// dotenv.config();
+// module.exports = {
+//     plugins: [
+//         new webpack.DefinePlugin({
+//             'process.env.REACT_APP_RECAPTCHA_SITE_KEY': JSON.stringify(process.env.REACT_APP_RECAPTCHA_SITE_KEY)
+//         })
+//     ]
+// };
 
-module.exports = {
-  apiKey: process.env.REACT_APP_API_KEY,
-};
+
+// require('dotenv').config();
+
+// module.exports = {
+//   apiKey: process.env.REACT_APP_RECAPTCHA_SITE_KEY,
+// };
