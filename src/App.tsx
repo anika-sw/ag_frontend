8f43d30b22ede37bb79fa4855fff63d3d32003dd
sunnybysunny <1sunnymuniz@gmail.com>
2024-08-08 10:36:19 -0600

    changed var name to sitekey

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DropdownMenu from './components/DropdownMenu';
 import GenerateSongButton from './components/GenerateSongButton';
 import './App.css';
 import ReCAPTCHA from "react-google-recaptcha";
+
+// declare global {
+//   interface Window {
+//     grecaptcha: {
+//       ready: (callback: () => void) => void;
+//       execute: (siteKey: string, params: { action: string }) => Promise<string>;
+//     };
+//   }
+// }
 
 const App: React.FC = () => {
   const [mood, setMood] = useState<string>('');
@@ -18,9 +27,31 @@ const App: React.FC = () => {
 
   const onChange = () => {};
 
+  
+  // function MyComponent() {
+  //   const [captchaToken, setCaptchaToken] = useState<string>('');
+  
+  //   useEffect(() => {
+  //     const onCaptchaChange = (token: string) => {
+  //       setCaptchaToken(token);
+  //     };
+  //     window.grecaptcha.ready(() => {
+  //       window.grecaptcha.execute('6LfI_RsqAAAAACEnGBBLuWFGY6HqSTJBRZd9A-sG', { action: 'homepage' }).then(onCaptchaChange);
+  //     });
+  //   }, []);
+  
+  //   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  //     event.preventDefault();
+  //     // Send captchaToken to your backend for verification
+  //   };
+  // }
+
+
   // i got the angry red line to go away by adding "vite-env.d.ts" to the tsconfig.json file"
-  const apiKey: string = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
-  console.log(apiKey);
+
+  const siteKey: string = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+  console.log("printing import.meta.env:", import.meta.env);
+  console.log("printing siteKey:", siteKey);
 
   // const apiKey = process.env.sitekey
 
@@ -38,7 +69,11 @@ const App: React.FC = () => {
       <DropdownMenu placeholder="Genre" options={["rock", "pop", "edm", "hiphop", "country"]} onSelect={handleSelect('genre')} />
       <DropdownMenu placeholder="Tempo" options={["slow", "medium", "fast"]} onSelect={handleSelect('tempo')} />
       <DropdownMenu placeholder="Mood" options={["happy", "sad", "angry", "romantic", "euphoric"]} onSelect={handleSelect('mood')} />
-      <ReCAPTCHA sitekey={apiKey} onChange={onChange} />
+      <ReCAPTCHA sitekey={siteKey} onChange={onChange} />
+      {/* <form onSubmit={handleSubmit}> */}
+        {/* Your form elements */}
+        {/* <button type="submit">Submit</button> */}
+      {/* </form> */}
       <GenerateSongButton genre={genre} mood={mood} tempo={tempo} setSongUrl={setSongUrl} />
       <audio src={songUrl} controls data-testid="audio-player"/>
     </div>
