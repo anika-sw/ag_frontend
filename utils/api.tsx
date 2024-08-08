4db7c4499898e4e1f4e174a6cd0d2d9794d1b106
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2024-08-08 16:00:04 -0700

    updated backend location url

--- utils/api.tsx
+++ utils/api.tsx
@@ -1,5 +1,6 @@
 export const apiCall1 = async (genre: string, mood: string, tempo: string) => {
-    const response = await fetch('http://flask-env.eba-np4xpmaq.us-east-2.elasticbeanstalk.com/create_song', {
+    const response = await fetch('https://ag-backend-caij.onrender.com/create_song', {
+    // const response = await fetch('http://flask-env.eba-np4xpmaq.us-east-2.elasticbeanstalk.com/create_song', {
     // const response = await fetch('http://localhost:5000/create_song', {
         method: 'POST',
         headers: {
@@ -20,7 +21,8 @@ export const apiCall1 = async (genre: string, mood: string, tempo: string) => {
 
 
 export const apiCall2 = async (genre: string, mood: string, tempo: string) => {
-    const response = await fetch('http://flask-env.eba-np4xpmaq.us-east-2.elasticbeanstalk.com/create_song_name', {
+    const response = await fetch('https://ag-backend-caij.onrender.com/create_song_name', {
+    // const response = await fetch('http://flask-env.eba-np4xpmaq.us-east-2.elasticbeanstalk.com/create_song_name', {
         // const response = await fetch('http://localhost:5000/create_song_name', {
         method: 'POST',
         headers: {
