375c0ab51da261c87831124c0a82cf644406b8de
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2024-08-13 19:01:55 -0700

    Attempt to make the logobar scroll more seamless

--- src/components/LogoBar.tsx
+++ src/components/LogoBar.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import './LogoBar.css';
 
 const LogoBar: React.FC = () => {
-  const sliderRef = useRef<HTMLDivElement | null>(null);
-
   const logos: string[] = [
     '/assets/youtube.svg',
     '/assets/twitch.svg',
@@ -14,34 +12,21 @@ const LogoBar: React.FC = () => {
     '/assets/and_more.svg'
   ];
 
-  useEffect(() => {
-    const slider = sliderRef.current;
-
-    if (slider) {
-      let currentScrollPos = 0;
-
-      const scroll = () => {
-        currentScrollPos += 1;
-        if (currentScrollPos >= slider.scrollWidth / 2) {
-          currentScrollPos = 0; // Reset scroll position to achieve the infinite loop effect
-        }
-        slider.scrollLeft = currentScrollPos;
-        requestAnimationFrame(scroll);
-      };
-
-      scroll();
-    }
-  }, []);
-
   return (
     <div className="logo-container">
-      <div className="logo-slider" ref={sliderRef}>
-        {[...logos, ...logos].map((logo, index) => (
-          <img src={logo} alt={`Logo ${index + 1}`} className="logo" key={index} />
-        ))}
+      <div className="logo-slider">
+        <div className="logos">
+          {logos.map((logo, index) => (
+            <img src={logo} alt={`Logo ${index + 1}`} className="logo" key={index} />
+          ))}
+          {logos.map((logo, index) => (
+            <img src={logo} alt={`Logo ${index + logos.length}`} className="logo" key={index + logos.length} />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default LogoBar;
+
